304e44b707ee4bf9a51ecda0ccc2f38357057bb7
Ivan <ichen48@my.bcit.ca>
2020-11-25 18:58:02 -0800

    Cleaned up test cases, removed backup

--- personal_website/js/script.js
+++ personal_website/js/script.js
@@ -1,24 +1,20 @@
-//
-
+// Javascript for courses.html
 
 
 function getInput(){
     let input = prompt("Please enter a 4-digit number: ");
     while ((!Number(input)) || (input.length != 4)){
-        input = prompt("Invalid input. Please enter a 4-digit number: ")
+        input = prompt("Invalid input. Please enter a 4-digit number: ");
     }
-    return input
+    return input;
 }
 
 
 function scanArray(input, array){
     for (let object of array){
-        alert(object.code + input)
-        alert(object.code.includes(input))
         if (object.code.includes(input)){
-            console.log(`Yes I am taking the course: ` + object.code + ` - `+ object.name)
-            alert(`Yes I am taking the course: ` + object.code + ` - `+ object.name)
-            return true
+            console.log(`Yes I am taking the course: ` + object.code + ` - `+ object.name);
+            return true;
         }
     }
 }
@@ -30,14 +26,13 @@ function main(){
         {code: "ACIT 1630",  name: "Database Systems"},
         {code: "ACIT 1515",  name: "Scripting for IT"},
     ]
-    input = getInput()
+    input = getInput();
     if (!scanArray(input, courseList)){
-        let newObj = {code: input, name: null}
-        alert(newObj.code + newObj.name)
-        console.log("New object successfully logged")
-        courseList.push(newObj)
-        alert(courseList[3].code)
+        let newObj = {code: input, name: null};
+        console.log("New object successfully logged");
+        courseList.push(newObj);
     }
 }
+
 
 main()
